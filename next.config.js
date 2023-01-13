c252596cce59363378a3f28df131373b5ca5237b
SnowFireWolf <dev@sfirew.com>
2023-01-13 15:59:49 +0800

    init linaria

--- next.config.js
+++ next.config.js
@@ -1,6 +1,56 @@
 /** @type {import('next').NextConfig} */
+function traverse(rules) {
+  for (let rule of rules) {
+    if (typeof rule.loader === 'string' && rule.loader.includes('css-loader')) {
+      if (
+        rule.options &&
+        rule.options.modules &&
+        typeof rule.options.modules.getLocalIdent === 'function'
+      ) {
+        let nextGetLocalIdent = rule.options.modules.getLocalIdent;
+        rule.options.modules.mode = 'local';
+        rule.options.modules.auto = true;
+        rule.options.modules.exportGlobals = true;
+        rule.options.modules.exportOnlyLocals = false;
+        rule.options.modules.getLocalIdent = (context, _, exportName, options) => {
+          if (context.resourcePath.includes(LINARIA_EXTENSION)) {
+            return exportName;
+          }
+          return nextGetLocalIdent(context, _, exportName, options);
+        };
+      }
+    }
+    if (typeof rule.use === 'object') {
+      traverse(Array.isArray(rule.use) ? rule.use : [rule.use]);
+    }
+    if (Array.isArray(rule.oneOf)) {
+      traverse(rule.oneOf);
+    }
+  }
+}
+
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, options) => {
+    traverse(config.module.rules);
+
+    config.module.rules.push({
+      test: /\.(tsx|ts|js|mjs|jsx)$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@linaria/webpack-loader',
+          options: {
+            sourceMap: process.env.NODE_ENV !== 'production',
+            extension: '.linaria.module.css',
+          },
+        },
+      ],
+    });
+
+    return config
+  },
 }
 
 module.exports = nextConfig
