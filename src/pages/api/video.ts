47d178eb589c805cd4ccf56f622daef54193b659
SnowFireWolf <dev@sfirew.com>
2023-01-13 19:43:19 +0800

    update

--- src/pages/api/video.ts
+++ src/pages/api/video.ts
@@ -8,5 +8,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ src: "https://reurl.cc/58QGAV" })
+  // res.status(200).json({ src: "https://reurl.cc/58QGAV" })
+  res.status(200).json({ src: "https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt714eaee50b90fc27/62cc7dcc6a8fb133b0ff7e55/VALORANT_ANNO22_SHATTERED_16x9_27s.mp4" })
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,79 +66,145 @@ const TextLayout = styled.div`
 `;
 
 const Title = styled.div`
+  max-width: 375px;
   font-family: 'Noto Serif';
   font-style: normal;
   font-weight: 400;
-  font-size: 48px;
+  /* font-size: 32px; */
+  font-size: clamp(32px, 5vw, 48px);
+  /* font-size: max(32px, min(3.75vw, 48px)); */
   line-height: 65px;
   text-align: center;
+
+  @media screen and (min-width: 1024px) {
+    /* font-size: 48px; */
+  }
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 1024px;
+  /* position: relative; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transform: all ease 500ms;
 `;
 
 const ImageLayout = styled.div`
   width: 100%;
+  max-width: 375px;
+  height: 1024px;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media screen and (min-width: 1024px) {
+    /* max-width: 100%; */
+  }
 `;
 
 const ImageBox = styled.div`
-  position: absolute;
+  position: relative;
   background-color: #aaaaaa;
   background-repeat: no-repeat;
-  background-size: cover;
   background-position: center;
+  background-size: 100%;
+  transition: all ease 480ms;
 
   &.i-1 {
     background-image: url("/assets/1.png");
 
-    width: 332px;
-    height: 259px;
+    width: 152px;
+    height: 112px;
+    left: 36px;
+    top: 36px;
 
-    top: 43px;
-    left: 472px;
+    @media screen and (min-width: 1024px) {
+      width: 332px;
+      height: 259px;
+      top: 43px;
+      left: 472px;
+    }
   }
 
   &.i-2 {
     background-image: url("/assets/2.png");
+    width: 81px;
+    height: 66px;
+    left: 263px;
+    top: 205px;
 
-    width: 172px;
-    height: 132px;
-
-    top: 150px;
-    left: 995px;
+    @media screen and (min-width: 1024px) {
+      width: 172px;
+      height: 132px;
+      left: 995px;
+      top: 150px;
+    }
   }
 
   &.i-3 {
     background-image: url("/assets/3.png");
+    width: 128px;
+    height: 99px;
+    left: 0px;
+    top: 221px;
+    /* top: calc(221px - (36px + 112px)); */
 
-    width: 231px;
-    height: 184px;
-    left: 50px;
-    top: 210px;
+    @media screen and (min-width: 1024px) {
+      width: 231px;
+      height: 184px;
+      left: 50px;
+      top: 210px;
+    }
   }
 
   &.i-4 {
     background-image: url("/assets/4.png");
 
-    width: 200px;
-    height: 210px;
-    left: 1258px;
-    top: 440px;
+    width: 126px;
+    height: 132px;
+    left: 345px;
+    top: 271px;
+
+    @media screen and (min-width: 1024px) {
+      width: 200px;
+      height: 210px;
+      left: 1258px;
+      top: 440px;
+    }
   }
 
   &.i-5 {
     background-image: url("/assets/5.png");
 
-    width: 413px;
-    height: 290px;
-    left: 0px;
-    top: 500px;
+    width: 207px;
+    height: 175px;
+    left: -155px;
+    top: 496px;
+
+    @media screen and (min-width: 1024px) {
+      width: 413px;
+      height: 290px;
+      left: 0px;
+      top: 500px;
+    }
   }
 
   &.i-6 {
     background-image: url("/assets/6.png");
 
-    width: 355px;
-    height: 285px;
-    left: 903px;
-    top: 650px;
+    width: 215px;
+    height: 162px;
+    left: 160px;
+    top: 584px;
+
+    @media screen and (min-width: 1024px) {
+      width: 355px;
+      height: 285px;
+      left: 903px;
+      top: 650px;
+    }
   }
 `;
 
@@ -183,7 +249,15 @@ export default function HomePage() {
                 <VideoLoadingSkeleton />
               ) : (
                 <VideoBanner>
-                  <video width="100%" height="100%" controls>
+                  <video
+                    width="100%"
+                    height="100%"
+                    preload="true"
+                    muted
+                    loop
+                    playsInline
+                    autoPlay
+                  >
                     <source src={videoSrc} type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
@@ -201,14 +275,16 @@ export default function HomePage() {
               </Title>
             </TextLayout>
 
-            <ImageLayout>
-              <ImageBox className="i-1" />
-              <ImageBox className="i-2" />
-              <ImageBox className="i-3" />
-              <ImageBox className="i-4" />
-              <ImageBox className="i-5" />
-              <ImageBox className="i-6" />
-            </ImageLayout>
+            <ImageWrapper>
+              <ImageLayout>
+                <ImageBox className="i-1" />
+                <ImageBox className="i-2" />
+                <ImageBox className="i-3" />
+                <ImageBox className="i-4" />
+                <ImageBox className="i-5" />
+                <ImageBox className="i-6" />
+              </ImageLayout>
+            </ImageWrapper>
           </Container>
         </Wrapper>
       </main>
